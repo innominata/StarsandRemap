c82776b9ab421a266b1e9e690f12316a69257e3c
innominata <inno@xray.cat>
2021-11-21 23:08:43 +1300

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UltimateSurvival;
+using UltimateSurvival.InputSystem;
+using UltimateSurvival.StandardAssets;
+using UnityEngine;
+using UnityEngine.UI;
+using Button = UltimateSurvival.InputSystem.Button;
+
+namespace StarsandRemap
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class StarsandRemap : BaseUnityPlugin
+    {
+        public static AssetBundle ab = AssetBundle.LoadFromFile(Path.GetDirectoryName(Assembly.GetAssembly(typeof(StarsandRemap)).Location) + "\\font");
+        public static Font font;
+        public static ManualLogSource Logger;
+        private void Awake()
+        {
+            Logger = new ManualLogSource("Remap");
+            BepInEx.Logging.Logger.Sources.Add(Logger);
+            Logger.LogInfo("Starsand Remap Mod Loading");
+            Harmony.CreateAndPatchAll(typeof(StarsandRemap));
+            if(ab != null)
+            {
+                font = ab.LoadAsset<Font>("Assets/Komon.ttf");
+                
+            }
+            else
+            {
+                Debug.Log("FAILED TO LOAD ASSET BUNDLE");
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PieceBuild), "Start")]
+        public static void PieceBuildStart(PieceBuild __instance)
+        {
+            Button jDestroy = GameController.InputManager.FindButton("JDestroy");
+            if (jDestroy != null)
+            {
+                var index = GameController.InputManager.m_InputData.Buttons.IndexOf(jDestroy);
+                GameController.InputManager.m_InputData.Buttons[index] = new Button("JDestroy", KeyCode.V);
+            }
+            else
+            {
+                jDestroy = new Button("JDestroy", KeyCode.V);
+                GameController.InputManager.AddButton(jDestroy);
+            }
+        }
+       
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Starsand Remap
+ 
+BepInEx mod for Starsand to change key binds. Work in progress
+
+## Installation
+
+### Mod Manager (Recommended)
+
+Install either [Thunderstore Mod Manager](https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager) or [r2modman](https://thunderstore.io/package/ebkr/r2modman/) and then install [StarsandRemap from Thunderstore](https://starsand.thunderstore.io/package/innominata/StarsandRemap/)
+
+### Manual Installation
+
+Follow the instructions from the [BepInEx Docs](https://docs.bepinex.dev/articles/user_guide/installation/index.html) to install BepInEx and then extract [the latest release](https://github.com/innominata/StarsandRemap/releases/latest) of the mod to your `BepInEx\plugins\` directory.
+
+## Configuration
+
+None required. Remove the mod or rename the .dll extension to .dllOld to disable
+
+## Support
+
+For support, or to discuss and discover other mods for Starsand, check out the [Starsand Modding Discord](https://discord.gg/ZYVpC6uyY7)
+
+## Changelog
+
+
+1.0.0:
+
+- Initial release. Simply changes Y to V. Configs coming soon!
